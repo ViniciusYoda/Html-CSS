6324acf23cd190e41f646a97edce80d273e02c1b
ViníciusYoda <rm92969@fiap.com.br>
2022-06-07 14:38:10 -0300

    mudando

--- exercicios/ex037/js/script.js
+++ exercicios/ex037/js/script.js
@@ -3,7 +3,8 @@ const botaoEnviar = document.getElementById("enviar")
 botaoEnviar.addEventListener("click", ()=>{
     const campos = document.querySelectorAll("input[type='text'], input[type='email'], input[type='password']")
 
-    for(let i = 0; i < campos.length; i++){
+    for(let i = 0; i < 
+        campos.length; i++){
         if(campos[i].value == ""){
             alert("O campo " + campo[i].name + " está sem preenchimento!")
             return
